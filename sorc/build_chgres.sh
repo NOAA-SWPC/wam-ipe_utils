ebcd0b9611436a5652b58c3603b47408a0c0fff9
Adam Kubaryk <Adam.Kubaryk@noaa.gov>
2021-03-05 21:09:19 +0000

    Changes for WCOSS Dell P3

--- sorc/build_chgres.sh
+++ sorc/build_chgres.sh
@@ -4,10 +4,13 @@ set -eux
 source ./machine-setup.sh > /dev/null 2>&1
 cwd=`pwd`
 
+module purge
+
 USE_PREINST_LIBS=${USE_PREINST_LIBS:-"true"}
 if [ $USE_PREINST_LIBS = true ]; then
   export MOD_PATH
-  source ../modulefiles/module_base.$target             > /dev/null 2>&1
+  module use ../modulefiles
+  module load module_base.$target             > /dev/null 2>&1
 else
   export MOD_PATH=${cwd}/lib/modulefiles
   if [ $target = wcoss_cray ]; then
